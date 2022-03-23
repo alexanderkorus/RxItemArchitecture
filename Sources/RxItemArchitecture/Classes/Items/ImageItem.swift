95704a2aeba1593dfb68dcc7e22d82ed751c192b
Alexander Korus <alexander.korus@svote.io>
2022-03-23 10:13:26 +0100

    added properties to image item for top and bottom space

--- Sources/RxItemArchitecture/Classes/Items/ImageItem.swift
+++ Sources/RxItemArchitecture/Classes/Items/ImageItem.swift
@@ -20,14 +20,18 @@ public struct ImageItem: BaseItem {
 	public var sideSpaces: CGFloat = 20.0
 	public var height: CGFloat = 100.0
 	public var tintColor: UIColor? = nil
+	public var topSpace: CGFloat = 10.0
+	public var bottomSpace: CGFloat = 10.0
 
-	public init(image: UIImage? = nil, contentMode: UIView.ContentMode = .scaleAspectFit, clipsToBounds: Bool = true, sideSpaces: CGFloat = 20.0, height: CGFloat = 100.0, tintColor: UIColor? = nil) {
+	public init(image: UIImage? = nil, contentMode: UIView.ContentMode = .scaleAspectFit, clipsToBounds: Bool = true, sideSpaces: CGFloat = 20.0, height: CGFloat = 100.0, tintColor: UIColor? = nil, topSpace: CGFloat = 10.0, bottomSpace: CGFloat = 10.0) {
 		self.image = image
 		self.contentMode = contentMode
 		self.clipsToBounds = clipsToBounds
 		self.sideSpaces = sideSpaces
 		self.height = height
 		self.tintColor = tintColor
+		self.topSpace = topSpace
+		self.bottomSpace = bottomSpace
 	}
 }
 
@@ -70,10 +74,10 @@ open class ImageCell: UITableViewCell, BaseCell {
 		}
 
 		self.localImageView.snp.remakeConstraints {
-			$0.top.equalToSuperview().offset(10.0)
+			$0.top.equalToSuperview().offset(item.topSpace)
 			$0.leading.equalToSuperview().offset(item.sideSpaces)
 			$0.trailing.equalToSuperview().offset(-item.sideSpaces)
-			$0.bottom.equalToSuperview().offset(-10.0)
+			$0.bottom.equalToSuperview().offset(-item.bottomSpace)
 			$0.height.equalTo(item.height)
 		}
 	}
